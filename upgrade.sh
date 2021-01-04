228be9343601a18f45cd4972ce8931eb04467959
Doug <noone@localhost>
2021-01-03 22:37:37 -0500

    Per-host config

--- upgrade.sh
+++ upgrade.sh
@@ -1,4 +1,15 @@
 #!/usr/bin/env bash
 sudo apt update
-sudo apt dist-upgrade
-sudo reboot
+sudo apt install nmap
+host=`hostname -s`
+if [ $host = "scanner" ]; then
+  sudo apt install nmap
+fi
+if [ $host = "target" ]; then
+  cd /users/deverso
+  git clone https://github.com/0xd0ug/clayPigeons.git
+fi
+if [ $host = "internet" ]; then
+  sudo tc qdisc add dev eth1 root netem delay 20ms
+  sudo tc qdisc add dev eth2 root netem delay 20ms
+fi
