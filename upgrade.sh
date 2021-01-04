c0ec82c4ac86a29ed143e2430b5f7632d1f9807c
Doug <noone@localhost>
2021-01-03 22:49:43 -0500

    bug fix

--- upgrade.sh
+++ upgrade.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
-sudo apt update
-sudo apt install nmap
 host=`hostname -s`
 if [ $host = "scanner" ]; then
-  sudo apt install nmap
+  sudo apt update
+  sudo apt -y install nmap
 fi
 if [ $host = "target" ]; then
   cd /users/deverso
