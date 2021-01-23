2b6dcd2e92f24debc0afff61f9d5e5244eb6d6bf
0xd0ug <32918088+0xd0ug@users.noreply.github.com>
2021-01-23 10:21:22 -0500

    Update upgrade.sh

--- upgrade.sh
+++ upgrade.sh
@@ -1,17 +1,25 @@
 #!/usr/bin/env bash
+# Must run as root
 hostname
 whoami
-sudo whoami
 host=`hostname -s`
+
+# Begin commands for all hosts
+
+apt update
+
+# End commands for all hosts
+
 if [ $host = "scanner" ]; then
-  sudo apt update
-  sudo apt -y install nmap
+  apt -y install nmap
 fi
 if [ $host = "target" ]; then
-  sudo git clone https://github.com/0xd0ug/clayPigeons.git /users/deverso/clayPigeons
-  sudo chown -R deverso /users/deverso/clayPigeons/
+  apt -y install pip3
+  pip3 install exrex
+  git clone https://github.com/0xd0ug/clayPigeons.git /users/deverso/clayPigeons
+  chown -R deverso /users/deverso/clayPigeons/
 fi
 if [ $host = "internet" ]; then
-  sudo tc qdisc add dev eth1 root netem delay 20ms
-  sudo tc qdisc add dev eth2 root netem delay 20ms
+  tc qdisc add dev eth1 root netem delay 20ms
+  tc qdisc add dev eth2 root netem delay 20ms
 fi
