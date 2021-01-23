dfbdd508c36c7f2ea9502bcb0de5f452b369af99
0xd0ug <32918088+0xd0ug@users.noreply.github.com>
2021-01-23 09:35:58 -0500

    fix sudo issue

--- profile.py
+++ profile.py
@@ -22,7 +22,7 @@ targetInternet = request.Link(members = [internet, fwtarget])
 scannerInternet = request.Link(members = [internet, fwscanner])
 
 # Install and execute a script that is contained in the repository.
-setupcmd = "bash /local/repository/upgrade.sh >/tmp/output 2>/tmp/error"
+setupcmd = "sudo bash /local/repository/upgrade.sh >/tmp/output 2>/tmp/error"
 scanner.addService(pg.Execute(shell="sh", command=setupcmd))
 target.addService(pg.Execute(shell="sh", command=setupcmd))
 internet.addService(pg.Execute(shell="sh", command=setupcmd))
